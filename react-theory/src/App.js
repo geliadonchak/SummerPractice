7d78c5f541f1ee787c60980b0bff4f3e07c48a07
Angelina Donchak <geliadonchak@mail.ru>
2020-07-20 17:56:52 +1000

    learn working with lists

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -29,7 +29,6 @@ class App extends Component {
             textAlign: 'center'
         }
 
-        const cars = this.state.cars
         return (
             <div style={divStyle}>
                 <h1>{this.state.pageTitle}</h1>
@@ -40,18 +39,17 @@ class App extends Component {
                     onClick={this.changeTitleHandler.bind(this, 'Changed!')}
                 >Change title</button>
 
-                <Car name={cars[0].name}
-                     year={cars[0].year}
-                     onChangeTitle = {this.changeTitleHandler.bind(this, cars[0].name)}
-                />
-                <Car name={cars[1].name}
-                     year={cars[1].year}
-                     onChangeTitle = {() => this.changeTitleHandler(cars[1].name)}
-                />
-                <Car name={cars[2].name}
-                     year={cars[2].year}
-                     onChangeTitle = {() => this.changeTitleHandler(cars[2].name)}
-                />
+                {this.state.cars.map((car, index) => {
+                    return (
+                        <Car
+                            key={index}
+                            name={car.name}
+                            year={car.year}
+                            onChangeTitle={() => this.changeTitleHandler(car.name)}
+                        />
+                    )
+                })}
+
             </div>
         );
     }
