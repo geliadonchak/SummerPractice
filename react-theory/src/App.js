db9b722d01f65c1ca0e85a7d32db6d355ea2ff84
Angelina Donchak <geliadonchak@mail.ru>
2020-07-20 17:16:44 +1000

    learn event handling

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -12,6 +12,14 @@ class App extends Component {
         pageTitle: 'React components'
     }
 
+    changeTitleHandler = () => {
+        const oldTitle = this.state.pageTitle
+        const newTitle = oldTitle + ' (changed)'
+        this.setState({
+            pageTitle:newTitle
+        })
+    }
+
     render () {
         const divStyle = {
             textAlign: 'center'
@@ -21,7 +29,9 @@ class App extends Component {
         return (
             <div style={divStyle}>
                 <h1>{this.state.pageTitle}</h1>
-                
+
+                <button onClick={this.changeTitleHandler}>Change title</button>
+
                 <Car name={cars[0].name} year={cars[0].year} />
                 <Car name={cars[1].name} year={cars[1].year} />
                 <Car name={cars[2].name} year={cars[2].year} />
