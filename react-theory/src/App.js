5ffd999e0464547841daa0c2774f5fdb7e5259f3
Angelina Donchak <geliadonchak@mail.ru>
2020-07-20 17:02:28 +1000

    learn state

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -3,20 +3,28 @@ import './App.css';
 import Car from './Car/Car'
 
 class App extends Component {
+    state = {
+        cars: [
+            {name: 'Ford', year: 2018},
+            {name: 'Audi', year: 2017},
+            {name: 'Mazda', year: 2019}
+        ],
+        pageTitle: 'React components'
+    }
+
     render () {
         const divStyle = {
             textAlign: 'center'
         }
 
+        const cars = this.state.cars
         return (
             <div style={divStyle}>
-                <Car name={'Ford'} year={2018}>
-                    <p style={{color: 'blue'}}>COLOR</p>
-                </Car>
-                <Car name="Audi" year={2017}>
-                    <p style={{color: 'red'}}>COLOR</p>
-                </Car>
-                <Car name={'Mazda'} year={2019}/>
+                <h1>{this.state.pageTitle}</h1>
+                
+                <Car name={cars[0].name} year={cars[0].year} />
+                <Car name={cars[1].name} year={cars[1].year} />
+                <Car name={cars[2].name} year={cars[2].year} />
             </div>
         );
     }
--- react-theory/src/Car/Car.js
+++ react-theory/src/Car/Car.js
@@ -4,6 +4,5 @@ export default props => (
     <div>
         <h3>Car name: {props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
-        {props.children}
     </div>
 );
