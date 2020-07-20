0d7d4fa4069a2e9ac2a7f472a411f85db8a697b3
Angelina Donchak <geliadonchak@mail.ru>
2020-07-20 17:33:01 +1000

    learn passing parameters to function

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -12,9 +12,7 @@ class App extends Component {
         pageTitle: 'React components'
     }
 
-    changeTitleHandler = () => {
-        const oldTitle = this.state.pageTitle
-        const newTitle = oldTitle + ' (changed)'
+    changeTitleHandler = (newTitle) => {
         this.setState({
             pageTitle:newTitle
         })
@@ -30,11 +28,22 @@ class App extends Component {
             <div style={divStyle}>
                 <h1>{this.state.pageTitle}</h1>
 
-                <button onClick={this.changeTitleHandler}>Change title</button>
+                <button
+                    onClick={this.changeTitleHandler.bind(this, 'Changed!')}
+                >Change title</button>
 
-                <Car name={cars[0].name} year={cars[0].year} />
-                <Car name={cars[1].name} year={cars[1].year} />
-                <Car name={cars[2].name} year={cars[2].year} />
+                <Car name={cars[0].name}
+                     year={cars[0].year}
+                     onChangeTitle = {this.changeTitleHandler.bind(this, cars[0].name)}
+                />
+                <Car name={cars[1].name}
+                     year={cars[1].year}
+                     onChangeTitle = {() => this.changeTitleHandler(cars[1].name)}
+                />
+                <Car name={cars[2].name}
+                     year={cars[2].year}
+                     onChangeTitle = {() => this.changeTitleHandler(cars[2].name)}
+                />
             </div>
         );
     }
--- react-theory/src/Car/Car.js
+++ react-theory/src/Car/Car.js
@@ -4,5 +4,6 @@ export default props => (
     <div>
         <h3>Car name: {props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
+        <button onClick={props.onChangeTitle}>Click</button>
     </div>
 );
