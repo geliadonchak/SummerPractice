9c878f24959f98079a249774a386549badfad228
Angelina Donchak <geliadonchak@mail.ru>
2020-07-21 11:41:02 +1000

    make dynamic classes

--- react-theory/src/components/Cars/Car.js
+++ react-theory/src/components/Cars/Car.js
@@ -2,11 +2,28 @@ import React from 'react'
 import './Car.css'
 
 function Car(props) {
+    const inputClasses = ['input']
+    
+    if (props.name !== '') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if (props.name.length > 4) {
+        inputClasses.push('bold')
+    }
+
     return (
         <div className="Car">
             <h3>Car name: {props.name}</h3>
             <p>Year: <strong>{props.year}</strong></p>
-            <input type="text" onChange={props.onChangeName} value={props.name}/>
+            <input
+                type="text"
+                onChange={props.onChangeName}
+                value={props.name}
+                className={inputClasses.join(' ')}
+            />
             <button onClick={props.onDelete}>Delete</button>
         </div>
     );
