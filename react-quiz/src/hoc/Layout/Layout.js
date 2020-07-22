a7176140c49a8e6296441c35079fc604a4eef976
Angelina Donchak <geliadonchak@mail.ru>
2020-07-22 15:22:47 +1000

    make simple menu

--- react-quiz/src/hoc/Layout/Layout.js
+++ react-quiz/src/hoc/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import classes from './Layout.css';
 import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle';
+import Drawer from '../../components/Navigation/Drawer/Drawer';
 
 class Layout extends Component {
     state = {
@@ -16,6 +17,11 @@ class Layout extends Component {
     render() {
         return (
             <div className={classes.Layout}>
+
+                <Drawer
+                    isOpen={this.state.menu}
+                />
+
                 <MenuToggle
                     onToggle={this.handleToggleMenu}
                     isOpen={this.state.menu}
