39a6a15d4dee9ede82d72d11c62e2dbf84b249b6
Angelina Donchak <geliadonchak@mail.ru>
2020-07-23 19:32:58 +1000

    add authorization page

--- react-quiz/src/components/FinishedQuiz/FinishedQuiz.js
+++ react-quiz/src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './FinishedQuiz.css';
 import Button from '../Ui/Button/Button';
+import {Link} from 'react-router-dom';
 
 const FinishedQuiz = props => {
     const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -34,7 +35,9 @@ const FinishedQuiz = props => {
 
             <div>
                 <Button onClick={props.onRetry} type="primary">Повторить</Button>
-                <Button type="success">Перейти в список тестов</Button>
+                <Link to='/'>
+                    <Button type="success">Перейти в список тестов</Button>
+                </Link>
             </div>
         </div>
     );
--- react-quiz/src/containers/Auth/Auth.js
+++ react-quiz/src/containers/Auth/Auth.js
@@ -1,10 +1,43 @@
 import React, {Component} from 'react';
+import classes from './Auth.css';
+import Button from '../../components/Ui/Button/Button';
 
 export default class Auth extends Component {
+    handleLogin = () => {
+
+    }
+
+    handleRegister = () => {
+
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit={this.handleSubmit} className={classes.AuthForm}>
+                        <input type="text"/>
+                        <input type="text"/>
+
+                        <Button type="success"
+                                onClick={this.handleLogin}
+                        >
+                            Войти
+                        </Button>
+
+                        <Button type="primary"
+                                onClick={this.handleRegister}
+                        >
+                            Зарегистрироваться
+                        </Button>
+                    </form>
+                </div>
             </div>
         );
     }
