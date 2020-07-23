c0f0903984cc867796028533d2492435c3e785bd
Angelina Donchak <geliadonchak@mail.ru>
2020-07-23 18:05:12 +1000

    add list page

--- react-quiz/src/containers/Quiz/Quiz.js
+++ react-quiz/src/containers/Quiz/Quiz.js
@@ -91,6 +91,10 @@ class Quiz extends Component {
         });
     }
 
+    componentDidMount() {
+        console.log('Quiz ID =', this.props.match.params.id);
+    }
+
     render() {
         return (
             <div className={classes.Quiz}>
--- react-quiz/src/containers/QuizList/QuizList.js
+++ react-quiz/src/containers/QuizList/QuizList.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
+import classes from './QuizList.css';
+import {NavLink} from 'react-router-dom';
 
 export default class QuizList extends Component {
+    renderQuizes() {
+        return [1, 2, 3].map((quiz, index) => {
+            return (
+                <li key={index}>
+                    <NavLink to={'/quiz/' + quiz}>
+                        Тест {quiz}
+                    </NavLink>
+                </li>
+            );
+        });
+    }
+
     render() {
         return (
-            <div>
-                <h1>Quiz List</h1>
+            <div className={classes.QuizList}>
+                <div>
+                    <h1>Список тестов</h1>
+
+                    <ul>
+                        {this.renderQuizes()}
+                    </ul>
+                </div>
             </div>
         );
     }
