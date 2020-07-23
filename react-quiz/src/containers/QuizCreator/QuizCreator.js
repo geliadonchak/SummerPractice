fc4dd57fc8d6f1962e88b8a4719ac39831ec13f2
Angelina Donchak <geliadonchak@mail.ru>
2020-07-23 23:25:22 +1000

    make create test page

--- react-quiz/src/containers/QuizCreator/QuizCreator.js
+++ react-quiz/src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,49 @@
 import React, {Component} from 'react';
+import classes from './QuizCreator.css';
+import Button from '../../components/Ui/Button/Button';
 
 export default class QuizCreator extends Component {
+    handleSubmit = event => {
+        event.preventDefault();
+    }
+
+    handleAddQuestion = () => {
+
+    }
+
+    handleCreateQuiz = () => {
+
+    }
+
     render() {
         return (
-            <div>
-                <h1>Quiz Creator</h1>
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>Создание теста</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text"/>
+                        <hr />
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+
+                        <select></select>
+
+                        <Button
+                            type="primary"
+                            onClick={this.handleAddQuestion}
+                        >
+                            Добавить вопрос
+                        </Button>
+                        <Button
+                            type="success"
+                            onClick={this.handleCreateQuiz}
+                        >
+                            Создать тест
+                        </Button>
+                    </form>
+                </div>
             </div>
         );
     }
