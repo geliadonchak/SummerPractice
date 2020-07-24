202a1ffdf3a12b0c4011d4c693a18795451d0c87
Angelina Donchak <geliadonchak@mail.ru>
2020-07-25 01:14:08 +1000

    learn middleware

--- react-theory/src/index.js
+++ react-theory/src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './redux/rootReducer';
 import {Provider} from 'react-redux';
 
-const store = createStore(rootReducer);
+// function loggerMiddleware(store) {
+//     return function (next) {
+//         return function (action) {
+//             const result = next(action);
+//             console.log('Middleware', store.getState());
+//             return result;
+//         };
+//     };
+// }
+
+const loggerMiddleware = store => next => action => {
+    const result = next(action);
+    console.log('Middleware', store.getState());
+    return result;
+};
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 const app = (
     <Provider store={store}>
         <App />
