0013af57ea883d8272ec3d25006bda9eb2be897b
Angelina Donchak <geliadonchak@mail.ru>
2020-07-24 13:29:35 +1000

    make quiz list loading

--- react-quiz/src/containers/QuizList/QuizList.js
+++ react-quiz/src/containers/QuizList/QuizList.js
@@ -4,22 +4,38 @@ import {NavLink} from 'react-router-dom';
 import axios from 'axios';
 
 export default class QuizList extends Component {
+    state = {
+        quizes: [],
+    }
+
     renderQuizes() {
-        return [1, 2, 3].map((quiz, index) => {
+        return this.state.quizes.map((quiz) => {
             return (
-                <li key={index}>
-                    <NavLink to={'/quiz/' + quiz}>
-                        Тест {quiz}
+                <li key={quiz.id}>
+                    <NavLink to={'/quiz/' + quiz.id}>
+                        {quiz.name}
                     </NavLink>
                 </li>
             );
         });
     }
 
-    componentDidMount() {
-        axios.get('https://react-quiz-eebdb.firebaseio.com/quiz.json').then(response => {
-            console.log(response);
-        });
+    async componentDidMount() {
+        try {
+            const response = await axios.get('https://react-quiz-eebdb.firebaseio.com/quizes.json');
+
+            const quizes = [];
+            Object.keys(response.data).forEach((key, index) => {
+                quizes.push({
+                    id: key,
+                    name: `Тест №${index + 1}`
+                });
+            });
+
+            this.setState({quizes});
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     render() {
