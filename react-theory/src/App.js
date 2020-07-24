d97d47310ab452905f9b99faafbea6d55b56b084
Angelina Donchak <geliadonchak@mail.ru>
2020-07-24 17:31:43 +1000

    create new theory project

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,96 +1,26 @@
 import React, {Component} from 'react';
-import './App.css';
-import Car from './components/cars/Car';
-import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
-import Counter from './components/counter/Counter';
-
-export const ClickedContext = React.createContext(false);
+import './App.scss';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            clicked: false,
-            cars: [
-                {name: 'Ford', year: 2018},
-                {name: 'Audi', year: 2017},
-                {name: 'Mazda', year: 2019}
-            ],
-            pageTitle: 'React components',
-            showCars: false
-        }
+    state = {
+        counter: 0
     }
 
-    handleToggleCars = () => {
+    updateCounter(value) {
         this.setState({
-            showCars: !this.state.showCars
+            counter: this.state.counter + value
         });
-    }
-
-    handleChangeName = (name, index) => {
-        const car = this.state.cars[index];
-        car.name = name;
-        const cars = this.state.cars;
-        cars[index] = car;
-        this.setState({
-            cars: cars
-        });
-    }
-
-    handleDelete = (index) => {
-        const cars = this.state.cars;
-        cars.splice(index, 1);
-        this.setState({
-            cars: cars
-        });
-    }
-
-    UNSAFE_componentWillMount() {
-        console.log('App componentWillMount');
-    }
-
-    componentDidMount() {
-        console.log('App componentDidMount');
     }
 
     render() {
-        console.log('App render');
-
-        const divStyle = {textAlign: 'center'};
-
-        let carsList = null;
-        if (this.state.showCars) {
-            carsList = this.state.cars.map((car, index) => {
-                return (
-                    <ErrorBoundary key={index}>
-                        <Car
-                            name={car.name}
-                            year={car.year}
-                            index={index}
-                            onDelete={() => this.handleDelete(index)}
-                            onChangeName={event => this.handleChangeName(event.target.value, index)}
-                        />
-                    </ErrorBoundary>
-                );
-            })
-        }
-
         return (
-            <div style={divStyle}>
-                <h1>{this.props.title}</h1>
-                <ClickedContext.Provider value={this.state.clicked}>
-                    <Counter />
-                </ClickedContext.Provider>
+            <div className="App">
+                <h1>Счетчик <strong>{this.state.counter}</strong></h1>
+
                 <hr />
-                <button style={{marginTop: '20px'}} onClick={this.handleToggleCars}>Toggle cars</button>
-                <button onClick={() => this.setState({clicked: true})}>Change clicked</button>
-                <div style={{
-                    width: '400px',
-                    margin: 'auto',
-                    paddingTop: '20px'
-                }}>
-                    {carsList}
+                <div className="Actions">
+                    <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
+                    <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
                 </div>
             </div>
         );
--- react-theory/src/hoc/Auxiliary.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Auxiliary = (props) => {
-    return props.children;
-}
-
-export default Auxiliary;
