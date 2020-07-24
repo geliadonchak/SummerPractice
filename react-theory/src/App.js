4bf45c0ed9ec46777a8b09bc35a092038d1f4484
Angelina Donchak <geliadonchak@mail.ru>
2020-07-25 00:22:09 +1000

    learn passing parameters

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -19,6 +19,10 @@ class App extends Component {
                     <button onClick={this.props.onAdd}>Добавить 1</button>
                     <button onClick={this.props.onSub}>Вычесть 1</button>
                 </div>
+                <div className="Actions">
+                    <button onClick={() => this.props.onAddNumber(15)}>Добавить 15</button>
+                    <button onClick={() => this.props.onAddNumber(-17)}>Вычесть 17</button>
+                </div>
             </div>
         );
     }
@@ -33,7 +37,8 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
     return {
         onAdd: () => dispatch({type: 'ADD'}),
-        onSub: () => dispatch({type: 'SUB'})
+        onSub: () => dispatch({type: 'SUB'}),
+        onAddNumber: number => dispatch({type: 'ADD_NUMBER', payload: number})
     };
 }
 
--- react-theory/src/redux/rootReducer.js
+++ react-theory/src/redux/rootReducer.js
@@ -12,7 +12,11 @@ export default function rootReducer(state = initialState, action) {
             return {
                 counter: state.counter - 1
             };
+        case 'ADD_NUMBER':
+            return {
+                counter: state.counter + action.payload
+            };
+        default:
+            return state;
     }
-
-    return state;
 };
