43e308b8bb62839e02b809c738599046bd40969e
Angelina Donchak <geliadonchak@mail.ru>
2020-07-24 18:02:53 +1000

    learn react-redux

--- react-theory/src/index.js
+++ react-theory/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import rootReducer from './redux/rootReducer';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App title={'I am from props!'}/>, document.getElementById('root'));
+const store = createStore(rootReducer);
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 serviceWorker.unregister();
