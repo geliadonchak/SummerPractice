418187d6cf372eeb4390fdae12ab8a3733b5484a
Angelina Donchak <geliadonchak@mail.ru>
2020-07-25 02:07:27 +1000

    install devtools

--- react-theory/src/index.js
+++ react-theory/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from './redux/rootReducer';
 import {Provider} from 'react-redux';
 import reduxThunk from 'redux-thunk';
@@ -18,16 +18,23 @@ import reduxThunk from 'redux-thunk';
 //     };
 // }
 
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
 const loggerMiddleware = store => next => action => {
     const result = next(action);
     console.log('Middleware', store.getState());
     return result;
 };
 
-const store = createStore(rootReducer, applyMiddleware(
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(
     loggerMiddleware,
     reduxThunk
-));
+)));
 
 const app = (
     <Provider store={store}>
