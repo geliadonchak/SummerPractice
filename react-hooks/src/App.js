5f2f401ad3b0a7eac12e03cc664ba5c15f3ce396
Angelina Donchak <geliadonchak@mail.ru>
2020-07-27 16:34:11 +1000

    add routing

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import {Navbar} from './components/Navbar';
-import {Home} from './pages/home';
+import {Home} from './pages/Home';
+import {About} from './pages/About';
+import {Profile} from './pages/Profile';
 
 function App() {
     return (
-        <React.Fragment>
+        <BrowserRouter>
             <Navbar />
             <div className="container pt-4">
-                <Home />
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/profile/:name" component={Profile}/>
+                </Switch>
             </div>
-        </React.Fragment>
+        </BrowserRouter>
     );
 }
 
--- react-hooks/src/pages/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const About = () => {
-    return (
-        <div>
-            <h1>About page</h1>
-        </div>
-    );
-};
