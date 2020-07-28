3a178b57f4e8859ef82f631683c646f060304d8e
Angelina Donchak <geliadonchak@mail.ru>
2020-07-28 14:05:26 +1000

    make getting data from github

--- react-hooks/src/components/Search.js
+++ react-hooks/src/components/Search.js
@@ -1,16 +1,19 @@
 import React, {useContext, useState} from 'react';
 import {AlertContext} from "../context/alert/alertContext";
+import {GithubContext} from "../context/github/githubContext";
 
 export const Search = () => {
     const [value, setValue] = useState('');
     const {show} = useContext(AlertContext);
+    const github = useContext(GithubContext);
+
     const onSubmit = event => {
         if (event.key !== 'Enter') {
             return;
         }
 
         if (value.trim()) {
-            console.log('Make request with: ', value);
+            github.search(value.trim());
         } else {
             show('Введите данные пользователя!');
         }
--- react-hooks/src/context/github/githubState.js
+++ react-hooks/src/context/github/githubState.js
@@ -1,7 +1,11 @@
 import React, {useReducer} from 'react';
+import axios from 'axios';
 import {GithubContext} from './githubContext';
 import {githubReducer} from './githubReducer';
 import {CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING} from "../types";
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
 export const GithubState = ({children}) => {
     const initialState = {
@@ -16,9 +20,13 @@ export const GithubState = ({children}) => {
     const search = async value => {
         setLoading();
 
+        const response = await axios.get(
+            `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+        );
+
         dispatch({
             type: SEARCH_USERS,
-            payload: []
+            payload: response.data.items
         });
     };
 
