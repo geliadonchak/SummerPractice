a9d9badc5b86668a628bb833e4adbb9fe7017c8e
Angelina Donchak <geliadonchak@mail.ru>
2020-07-28 13:42:51 +1000

    create github state

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -6,22 +6,25 @@ import {About} from './pages/About';
 import {Profile} from './pages/Profile';
 import {Alert} from './components/Alert';
 import {AlertState} from './context/alert/alertState';
+import {GithubState} from './context/github/githubState';
 
 function App() {
     return (
-        <AlertState>
-            <BrowserRouter>
-                <Navbar />
-                <div className="container pt-4">
-                    <Alert alert={{text: 'Test alert'}}/>
-                    <Switch>
-                        <Route path="/" exact component={Home}/>
-                        <Route path="/about" component={About}/>
-                        <Route path="/profile/:name" component={Profile}/>
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        </AlertState>
+        <GithubState>
+            <AlertState>
+                <BrowserRouter>
+                    <Navbar />
+                    <div className="container pt-4">
+                        <Alert alert={{text: 'Test alert'}}/>
+                        <Switch>
+                            <Route path="/" exact component={Home}/>
+                            <Route path="/about" component={About}/>
+                            <Route path="/profile/:name" component={Profile}/>
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </AlertState>
+        </GithubState>
     );
 }
 
---
+++ react-hooks/src/context/github/githubState.js
@@ -0,0 +1,57 @@
+import React, {useReducer} from 'react';
+import {GithubContext} from './githubContext';
+import {githubReducer} from './githubReducer';
+import {CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING} from "../types";
+
+export const GithubState = ({children}) => {
+    const initialState = {
+        user: {},
+        users: [],
+        loading: false,
+        repos: []
+    };
+
+    const [state, dispatch] = useReducer(githubReducer, initialState);
+
+    const search = async value => {
+        setLoading();
+
+        dispatch({
+            type: SEARCH_USERS,
+            payload: []
+        });
+    };
+
+    const getUser = async name => {
+        setLoading();
+
+        dispatch({
+            type: GET_USER,
+            payload: {}
+        });
+    };
+
+    const getRepos = async name => {
+        setLoading();
+
+        dispatch({
+            type: GET_REPOS,
+            payload: []
+        })
+    };
+
+    const clearUsers = () => dispatch({type: CLEAR_USERS});
+
+    const setLoading = () => dispatch({type: SET_LOADING});
+
+    const {user, users, repos, loading} = state;
+
+    return (
+        <GithubContext.Provider value={{
+            setLoading, search, getUser, getRepos, clearUsers,
+            user, users, repos, loading
+        }}>
+            {children}
+        </GithubContext.Provider>
+    );
+};
