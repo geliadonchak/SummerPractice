25d927054b42219456cbd668878b13629a6de317
Angelina Donchak <geliadonchak@mail.ru>
2020-07-28 17:13:07 +1000

    make  user display

--- react-hooks/src/pages/Profile.js
+++ react-hooks/src/pages/Profile.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, {useContext, useEffect, Fragment} from 'react';
+import {GithubContext} from '../context/github/githubContext';
+import {Link} from 'react-router-dom';
 
-export const Profile = () => {
+export const Profile = ({match}) => {
+    const {getUser, getRepos, loading, user, repos} = useContext(GithubContext);
+    const urlName = match.params.name;
+
+    useEffect(() => {
+        getUser(urlName);
+        getRepos(urlName);
+        // eslint-disable-next-line
+    }, []);
+
+    if (loading) {
+        return <p className="text-center">Загрузка...</p>
+    }
+
+    const {
+        name, company, avatar_url,
+        location, bio, blog,
+        login, html_url, followers,
+        following, public_repos, public_gists
+    } = user;
+
     return (
-        <div>
-            <h1>Profile page</h1>
-        </div>
+        <Fragment>
+            <Link to="/" className="btn btn-link">На главную</Link>
+
+            <div className="card mb-4">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-3 text-center">
+                            <img
+                                src={avatar_url}
+                                alt={name}
+                                style={{width: '150px'}}
+                            />
+                            <h1>{name}</h1>
+                            {location && <p>Location: {location}</p>}
+
+                        </div>
+                        <div className="col">
+                            {
+                                bio && <Fragment>
+                                           <h3>BIO</h3>
+                                           <p>{bio}</p>
+                                       </Fragment>
+                            }
+                            <a href={html_url}
+                               target="_blank"
+                               rel="noopener noreferrer"
+                               className="btn btn-dark"
+                            >Открыть профиль</a>
+                            <ul>
+                                {login && <li><strong>Username: </strong> {login} </li>}
+                                {company && <li><strong>Company: </strong> {company} </li>}
+                                {blog && <li><strong>Website: </strong> {blog} </li>}
+                            </ul>
+
+                            <div className="badge badge-primary">Followers: {followers}</div>
+                            <div className="badge badge-success">Following: {following}</div>
+                            <div className="badge badge-info">Repositories: {public_repos}</div>
+                            <div className="badge badge-dark">Gists: {public_gists}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {repos.join()}
+        </Fragment>
     );
 };
