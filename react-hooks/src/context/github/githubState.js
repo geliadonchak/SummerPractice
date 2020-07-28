2fdac5bc2e8b987ccbd6cd7730c4f86af4b50ff7
Angelina Donchak <geliadonchak@mail.ru>
2020-07-28 14:16:58 +1000

    make request setting

--- react-hooks/src/context/github/githubState.js
+++ react-hooks/src/context/github/githubState.js
@@ -7,6 +7,10 @@ import {CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING} from "../ty
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
+const withCreds = url => {
+    return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+};
+
 export const GithubState = ({children}) => {
     const initialState = {
         user: {},
@@ -21,7 +25,7 @@ export const GithubState = ({children}) => {
         setLoading();
 
         const response = await axios.get(
-            `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+            withCreds(`https://api.github.com/search/users?q=${value}&`)
         );
 
         dispatch({
@@ -33,18 +37,26 @@ export const GithubState = ({children}) => {
     const getUser = async name => {
         setLoading();
 
+        const response = await axios.get(
+            withCreds(`https://api.github.com/users/${name}?`)
+        );
+
         dispatch({
             type: GET_USER,
-            payload: {}
+            payload: response.data
         });
     };
 
     const getRepos = async name => {
         setLoading();
 
+        const response = await axios.get(
+            withCreds(`https://api.github.com/users/${name}/repos?per_page=5&`)
+        );
+
         dispatch({
             type: GET_REPOS,
-            payload: []
+            payload: response.data
         })
     };
 
