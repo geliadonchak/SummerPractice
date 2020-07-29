f702e8e09983c3a5272b6df084c431cdda199718
Angelina Donchak <geliadonchak@mail.ru>
2020-07-29 11:40:04 +1000

    make animation with transition

--- react-animation/src/App.js
+++ react-animation/src/App.js
@@ -1,16 +1,27 @@
 import React, {useState} from 'react';
+import {Transition} from 'react-transition-group';
 
 export default function App() {
     const [toggle, setToggle] = useState(true);
 
     return (
         <div className="container">
-        <button onClick={() => setToggle(!toggle)}>Toggle</button>
-        <hr/>
-        <div className={'blocks'}>
-            {toggle && <div className="square blue">{toggle.toString()}</div>}
-        </div>
+            <button onClick={() => setToggle(!toggle)}>Toggle</button>
+            <hr/>
+            <div className={'blocks'}>
+                <Transition
+                    in={toggle}
+                    timeout={{
+                        enter: 1000,
+                        exit: 500
+                    }}
+                    mountOnEnter
+                    unmountOnExit
+                >
+                    {state => <div className={`square blue ${state}`}>{state}</div>}
+                </Transition>
+            </div>
         </div>
     );
-};
+}
 
