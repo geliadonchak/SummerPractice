ac32c653369436e537086151bde2b8c67d41c8f4
Angelina Donchak <geliadonchak@mail.ru>
2020-07-29 16:05:34 +1000

    make simple app

--- react-hoc/src/index.js
+++ react-hoc/src/index.js
@@ -2,13 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    state = {
+        starWarsChars: [
+            { name: 'Дарт Вэйдер', side: 'dark' },
+            { name: 'Люк Скайворкер', side: 'light' },
+            { name: 'Палпатин', side: 'dark' },
+            { name: 'Обиван Кеноби', side: 'light' }
+        ]
+    };
+
     render() {
         return (
-            <div>
-
-            </div>
+            <ul>
+                { this.state.starWarsChars.map((char, index) => {
+                    return (
+                        <li key={char.name + index}>
+                            <strong>{char.name}</strong> - &nbsp;
+                            {char.side}
+                        </li>
+                    )
+                }) }
+            </ul>
         );
     };
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
