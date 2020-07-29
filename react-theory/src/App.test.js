d66b8f6025201411b3db3145c5e2cac60e123536
Angelina Donchak <geliadonchak@mail.ru>
2020-07-29 15:07:03 +1000

    make tests creation

--- react-theory/src/App.test.js
+++ react-theory/src/App.test.js
@@ -1,11 +1,23 @@
-import {configure} from 'enzyme';
+import React from 'react';
+import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+import Character from './Character';
 
 configure({adapter: new Adapter()});
 
 describe('<App />', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<App />);
+    });
 
     it('Should render 3 characters in light side', () => {
+        expect(wrapper.find(Character)).toHaveLength(3);
+    });
 
+    it('Should render 2 characters in dark side', () => {
+        wrapper.setProps({side: 'dark'});
+        expect(wrapper.find(Character)).toHaveLength(2);
     });
 });
