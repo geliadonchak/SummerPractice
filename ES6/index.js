16bdbf40fc149b82302835edc673e30fa0a6095e
Angelina Donchak <geliadonchak@mail.ru>
2020-07-29 18:37:21 +1000

    learn default parameters

--- ES6/index.js
+++ ES6/index.js
@@ -1,24 +1,8 @@
-// const original = function () {
-//     return 100;
-// };
-// console.log(original());
+// const c = 90;
+const c2 = () => 200;
 
-// const arrow = () => {
-//     return 150;
-// };
-// console.log(arrow());
-
-// const arrow2 = (num, num2) => 150 + num *num2;
-// console.log(arrow2(50, 4));
-
-const obj = {
-    name: 'WFM',
-
-    logName: function () {
-        setTimeout(() => {
-            console.log('Name: ', this.name);
-        }, 2000)
-    }
+const func = (a = 20, b = a + c2()) => {
+    return a + b;
 };
 
-obj.logName();
+console.log(func(30));
