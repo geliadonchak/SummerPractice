5fc45d6997879887372f368b02f9300ca09de976
Angelina Donchak <geliadonchak@mail.ru>
2020-07-29 14:45:48 +1000

    set up test

---
+++ react-theory/src/App.test.js
@@ -0,0 +1,11 @@
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe('<App />', () => {
+
+    it('Should render 3 characters in light side', () => {
+
+    });
+});
