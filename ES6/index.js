439f4a78b98d3a41c0ae3daa0243b32bf4de4c84
Angelina Donchak <geliadonchak@mail.ru>
2020-07-29 18:29:36 +1000

    learn arrow functions

--- ES6/index.js
+++ ES6/index.js
@@ -1,8 +1,24 @@
-for (let i = 0; i < 5; i++) {
-    setTimeout(function () {
-        console.log(i);
-    }, 3000);
-}
+// const original = function () {
+//     return 100;
+// };
+// console.log(original());
 
-const PI = 3.14;
-let a = 1;
+// const arrow = () => {
+//     return 150;
+// };
+// console.log(arrow());
+
+// const arrow2 = (num, num2) => 150 + num *num2;
+// console.log(arrow2(50, 4));
+
+const obj = {
+    name: 'WFM',
+
+    logName: function () {
+        setTimeout(() => {
+            console.log('Name: ', this.name);
+        }, 2000)
+    }
+};
+
+obj.logName();
