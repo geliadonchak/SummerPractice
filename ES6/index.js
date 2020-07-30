8960df1245984c5443fe23eddf82a9f97a811133
Angelina Donchak <geliadonchak@mail.ru>
2020-07-30 16:14:11 +1000

    learn promise

--- ES6/index.js
+++ ES6/index.js
@@ -1,18 +1,53 @@
-// let obj1 = {a: 1};
-// let obj2 = {b: 2, c: 3};
-// Object.assign(obj1, obj2);
+// http://date.jsontest.com/
+
+import $ from 'jquery';
+
+// function oldDelay(ms, func) {
+//     setTimeout(function () {
+//         func();
+//     }, ms);
+// }
 //
-// let obj3 = Object.assign({d: 4}, obj1, obj2);
+// oldDelay(3000, function () {
+//     console.log('Old delay passed!');
+// });
+
+// function delay(ms = 1000) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve();
+//         }, ms);
+//     });
+// }
 //
-// console.log('Obj1', obj1);
-// console.log('Obj2', obj2);
-// console.log('Obj3', obj3);
+// delay(4000)
+//     .then(() => {
+//         console.log('new delay passed!');
+//     })
+//     .catch(() => {
+//         console.info('error');
+//     });
 
-// let findedItem = [1, 2, 3, 4].find(x => x > 2);
-// console.log(findedItem);
+let promise = new Promise((resolve, reject) => {
+    $.ajax({
+        url: 'http://d1ate.jsontest.com/',
+        dataType: 'json',
+        success: function(response) {
+            resolve(response);
+        },
+        error: function(error) {
+            reject(error);
+        }
+    });
+});
 
-let str = 'Hello!';
-
-console.log('Repeat: ', str.repeat(3));
-console.log('StartsWith: ', str.startsWith('el', 1));
-console.log('Includes: ', str.includes('llo', 2));
+promise
+    .then((data) => {
+        return data.date;
+    })
+    .then((date) => {
+        console.log('Date: ', date);
+    })
+    .catch((error) => {
+        console.info('Server error');
+    });
