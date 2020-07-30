9a9e18377dc13aa270ada78b75f73a3f322eceba
Angelina Donchak <geliadonchak@mail.ru>
2020-07-30 14:32:28 +1000

    learn module system

--- ES6/index.js
+++ ES6/index.js
@@ -1,40 +1,12 @@
-// let map = new Map();
-// map.set('name', 'WFM');
-// map.set('age', 20);
-// console.log(map);
-//
-// console.log(map.get('age'));
-//
-// let obj1 = {};
-// let obj2 = {};
-// map.set(obj1, 10);
-// map.set(obj2, 20);
-// console.log(map.size);
-//
-// console.log(map.has(obj2));
-//
-// map.delete('name');
-// console.log(map.size);
+// import * as extra from './extra';
+import {name, age} from './extra';
+// import {age as a} from './extra';
+// // import {func} from './extra';
+import func from './extra';
+import Car from './extra';
 
-// let map = new Map([
-//     ['name', 'WFM'],
-//     ['age', 20]
-// ]);
-// console.log(map);
-//
-// for (let value of map.values()) {
-//     console.log(value);
-// }
-// for (let key of map.keys()) {
-//     console.log(key);
-// }
-// for (let enter of map.entries()) {
-//     console.log(`${enter[0]} - ${enter[1]}`);
-// }
-
-let map = new WeakMap();
-let key = {};
-map.set(key, 'key');
-console.log('Size: ', map.size);
-key = null;
-console.log('Size: ', map.size);
+// console.log(extra);
+console.log(name, age);
+console.log(a);
+func();
+var car = new Car();
