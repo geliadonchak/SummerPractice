fd6277deb8bf798613216a4e1ecf08fed4fa3103
Angelina Donchak <geliadonchak@mail.ru>
2020-07-30 17:48:54 +1000

    introduction to js, learn syntax and variables

---
+++ JS/js/script.js
@@ -0,0 +1,17 @@
+var my_var = 10;
+
+var a = 10;
+
+var b = 10;
+var c = '20';
+
+/*10, 20
+'hello',"hello"
+true false
+
+null
+undefined*/
+
+c = 45;
+a = c + b; // 'hello' + 'world ' = 'helloworld'
+alert(a);
