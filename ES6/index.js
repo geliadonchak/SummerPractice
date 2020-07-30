e5a6a5c5cabc544cf43aea1f2444d80bcd642b64
Angelina Donchak <geliadonchak@mail.ru>
2020-07-30 15:55:27 +1000

    learn new methods

--- ES6/index.js
+++ ES6/index.js
@@ -1,63 +1,18 @@
-// function* gen() {
-//     yield 11;
-//     yield 22;
-//     yield 33;
-// }
+// let obj1 = {a: 1};
+// let obj2 = {b: 2, c: 3};
+// Object.assign(obj1, obj2);
 //
-// let iter = gen();
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-
-// function* g1() {
-//     yield 1;
-//     yield* g2();
-//     yield 4;
-// }
-// function* g2() {
-//     yield 2;
-//     yield 3;
-// }
+// let obj3 = Object.assign({d: 4}, obj1, obj2);
 //
-// let iter = g1();
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
+// console.log('Obj1', obj1);
+// console.log('Obj2', obj2);
+// console.log('Obj3', obj3);
 
-// function* g() {
-//     yield* [1, 2, 3];
-// }
-// let iter = g();
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
+// let findedItem = [1, 2, 3, 4].find(x => x > 2);
+// console.log(findedItem);
 
-// function* getRange(start = 0, end = 100, step = 10) {
-//     while (start < end) {
-//         yield start;
-//         start += step;
-//     }
-// }
-// for (let n of getRange(10, 50)) {
-//     console.log(n);
-// }
+let str = 'Hello!';
 
-// let fib = {
-//     *[Symbol.iterator]() {
-//         let cur = 1, pre = 0;
-//         for (;;) {
-//             [cur, pre] = [cur + pre, cur];
-//             yield cur;
-//         }
-//     }
-// };
-//
-//
-// for (let n of fib) {
-//     if (n > 3000) break;
-//     console.log(n);
-// }
+console.log('Repeat: ', str.repeat(3));
+console.log('StartsWith: ', str.startsWith('el', 1));
+console.log('Includes: ', str.includes('llo', 2));
