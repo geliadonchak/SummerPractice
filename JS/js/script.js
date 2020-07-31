3871d96084f964a17c78f39bd980b7983c94d550
Angelina Donchak <geliadonchak@mail.ru>
2020-07-31 16:19:23 +1000

    learn strings

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,54 +1,32 @@
-// var date = new Date(5678);
-// var date = new Date("04 25 2019");
-// var date2 = Date();
-// console.log(date);
-//
-// alert(date.getDate());
-// alert(date.getDay());
-// alert(date.getFullYear());
-// alert(date.getHours());
-// alert(date.getMilliseconds());
-// alert(date.getMinutes());
-// alert(date.getMonth());
-// alert(date.getSeconds());
-// alert(date.getTime());
-// alert(date.getTimezoneOffset());
-//
-// alert(date.setDate(10));
-// alert(date.setFullYear(2014));
-// alert(date.setHours(22));
-// alert(date.setMilliseconds(55));
-// alert(date.setMinutes(45));
-// alert(date.setMonth(11));
-// alert(date.setSeconds(33));
-// alert(date.setTime(456788457));
-//
-// console.log(Date.now);
-// console.log(Date.parse);
-// console.log(Date.toString());
+function stringWork() {
+    var str = document.getElementById('head').innerHTML;
 
-function displayTime() {
-    var now = new Date();
-    var div = document.getElementById("clock");
-    
-    div.innerHTML = now.toLocaleTimeString();
+    // var result = str;
+    // var result = new String(str);
+    // var result = str.charAt(10);
+    // var result = str[10];
+    // var result = str.charCodeAt(15);
+    // var result = str.concat('hello', '!!!');
+    // var result = String.fromCharCode(str.charCodeAt(15));
+    // var result = str.indexOf('JavaScript');
+    // var result = str.lastIndexOf('Script');
+    // var result = str.length;
+    // var result = str.localeCompare('A');
+    // var strings = ['hi', 'buy', 'my', 'tie'];
+    // var result = strings.sort;
+    // var result = str.match(/\+d/);
+    // var result = str.replace(/\+d/, 'Heeeello');
+    // var result = str.search(/\+d/);
+    // var result = str.slice(3, 7);
+    // var result = str.split('-');
+    // var result = str.substr(1, 6);
+    // var result = str.substring(1, 9);
+    // var result = str.toLowerCase();
+    // var result = str.toUpperCase();
 
-    setTimeout(displayTime, 1000);
+    document.getElementById('result').innerHTML = result;
 }
 
-function displayTime2() {
-    var now = new Date();
-    var div = document.getElementById("clock");
-    var sDate = new Date(2021, 11, 31);
-    var timer = sDate.getTime() - now.getTime();
-
-    var days = parseInt(timer / (24 * 60 * 60 * 1000));
-    var hours = parseInt(timer / (60 * 60 * 1000) % 24);
-    var minutes = parseInt(timer / (60 * 1000) % 60);
-    var seconds = parseInt(timer / 1000 % 60);
-
-    div.innerHTML = days + ' : ' + hours + ' : ' + minutes + ' : ' + seconds;
-    setTimeout(displayTime2, 1000);
+window.onload = function () {
+    stringWork();
 }
-
-window.onload = displayTime2;
