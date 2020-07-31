511140813242241cd02b8a74af7086236b098e20
Angelina Donchak <geliadonchak@mail.ru>
2020-07-31 11:48:14 +1000

    learn functions

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,118 +1,69 @@
-var a = 10;
-var b = 20;
+/*var result = 100;
 
-// 1, 2, 3 Infinity -Infinity 'string' => TRUE
-// 0 -0 null NaN Undefined => FALSE
-
-var c = 0;
-/*
-if (((a > b && 'hello' != 'string') || !false) && !(a - 10)) {
-
-}
-
-if (a > 0) {
-	if (b < 25) {
-		if (c == 0) {
-			c = b + a;
-			alert(c);
-		}
-	}
-}*/
-
-/*if (a) {
-
-	if (a > 10) {
-		alert('a > 10');
-	}
-	else {
-		alert('else');
+function func(param1,param2) {
+	if (param1 == 40) {
+		return;
 	}
 
-}*/
-
-/*if (a > 10) {
-	console.log('a > 10');
+	var result = param1 * param2;
+	return result;
+	//alert(result);
 }
-else if ( a < 10 ) {
-	console.log('a < 10');
-}
-else if ( a == 0) {
-	console.log('a == 0');
-}
-else {
-	console.log('else');
-}*/
 
+var a  = 10;
+var b  = 50;
 
-//alert(c);
+//func(a,a*b);
+//var res = func(a,a+b);
+result = func(a,b);
 
-/*switch (a + b) {
+console.log(result);*/
 
-	case 10:
-		alert('hello');
-		break;
+/*var c = 3;
 
-	case 30:
-		alert('20');
-		break;
+function func(param1,param2) {
 
-	default:
-		document.write('default');
-		break;
-}*/
+	var result = param1 * param2;
 
-/*if ((a + b) === 10) {
-	alert('hello');
-}
-else if ((a + b) === 30) {
-	alert('20');
-}
-else {
-	document.write('default');
-}*/
-
-/*var count = 0;
-
-while (count < 10) {
-	console.log(count);
-	count++;
-}*/
-
-/*var c = 2;
-var i = 0;
-
-
-do {
-	c = c*2;
-	i++;
-}
-while (i < 9);
-console.log(c)*/;
-
-/*var c = 2;
-var i = 0;
-
-for (;i < 10 ; i++) {
-	if (i == 5) {
-		continue;
+	function func2(a) {
+		return result * param2 * a * c;
 	}
-	console.log(i);
-}*/
 
-var str = '<table border="1px" width="30%">';
-
-for (var i = 1; i < 10; i++) {
-    str += '<tr>';
-
-    for (var j = 1; j < 10; j++) {
-        str += '<td>';
-        str += i*j;
-        str += '</td>';
-    }
-
-    str += '</tr>';
+	result = func2(2);
+	return result;
 }
-str += '</table>';
 
-var el = document.getElementById('karkas');
-el.innerHTML = el.innerHTML + str;
+document.write(func(10,30));*/
+
+/*(function foo(param1,param2) {
+	console.log(param1);
+}   (10,20));*/
+
+
+/*function func(x) {
+	if (x <= 1) {
+		return 1;
+	}
+
+	return  x * func(x-1);
+}
+
+console.log(func(3));//1*2*3*/
+
+/*var f = function fact(x) {
+	if ( x <= 1) {
+		return 1;
+	}
+
+	return  x * fact(x-1);
+};
+
+document.write(f(4));*/
+
+function func() {
+    //var result = param1 * param2;
+    var result = arguments[1] * arguments[3];
+    return result;
+}
+
+alert(func(10,20,63,23,25,34,5467,678));
